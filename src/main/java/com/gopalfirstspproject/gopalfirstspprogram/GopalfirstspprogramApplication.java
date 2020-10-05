e8505ed68b5357cd81383205bb74d04c1a841154
abhi <abhijeetvc7@gmail.com>
2020-10-05 18:04:17 +0530

    Initial commit

---
+++ src/main/java/com/gopalfirstspproject/gopalfirstspprogram/GopalfirstspprogramApplication.java
@@ -0,0 +1,33 @@
+package com.gopalfirstspproject.gopalfirstspprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController  // = @Controller + @ResponseBody
+@RequestMapping(value="/start")
+@SpringBootApplication  // = @Configuration, @EnableAutoConfiguration, @ComponentScan
+public class GopalfirstspprogramApplication {
+
+	public static void main(String[] args) {
+		//ConfigurableApplicationContext
+		SpringApplication.run(GopalfirstspprogramApplication.class, args);
+	}
+
+	@GetMapping(value="/check")
+	public String getData(){
+
+		return "First Spring program...";
+	}
+
+	@GetMapping(value="/add/{num1}/{num2}")
+	public Integer addNos(@PathVariable Integer num1,@PathVariable Integer num2){
+		return num1+num2;
+	}
+
+}
+
+
+
+
---
+++ src/test/java/com/gopalfirstspproject/gopalfirstspprogram/GopalfirstspprogramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gopalfirstspproject.gopalfirstspprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GopalfirstspprogramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
